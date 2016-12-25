256a88a43a0fe770e5011d0dec9ae73a6d7ec0c2
吴涛 <1059912278@qq.com>
2016-12-25 19:30:28 +0800

    milestone 1

--- patch-gen/javaweb-patch-gen/cmp.py
+++ patch-gen/javaweb-patch-gen/cmp.py
@@ -2,107 +2,145 @@ import filecmp
 import os
 import shutil
 import json
-
-
-# SCRIPT ENVIRONMENT
-
-# the default script workspace
-WORKSPACE = 'C:/Users/watertao/Desktop/workspace'
-# the default absolute path of last production build
-DFT_BASE_BUILD_ABS_PATH = os.path.join(WORKSPACE, 'original')
-# the default absolute path of this production build
-DFT_TARGET_BUILD_ABS_PATH = os.path.join(WORKSPACE, 'target')
-# the default absolute path where patch to output
-DFT_PATCH_ABS_PATH = os.path.join(WORKSPACE, 'patch')
-
-# base url of svn
-SVN_URL_PREFIX = 'http://10.10.10.176:88/svn/accountcenter/Code/accountcenter/'
-SVN_BASE_BUILD_URL = SVN_URL_PREFIX + 'branches/2.9.7.4/'
+import subprocess
 
 
 def main():
 
-    CONFIG = json.load(open('config.json'))
+    config = json.load(open('config.json'))
 
-    print(CONFIG['workspace']['is_purge'])
+    init_workspace(config['workspace'])
 
-    print(123)
-    # if initworkspace
-
-    # svn checkout
-
-
-    # build base version
-
-
-    # build target version
+    for approach in config['approaches']:
+        if approach['ignore']:
+            continue
+        processor_class = eval(approach['processor'])
+        processor = processor_class(config['workspace']['path'], approach['configuration'])
+        processor.process()
 
 
+# initialize workspace directory (just clean the directory)
+def init_workspace(config):
+    if not os.path.exists(config['path']):
+        os.makedirs(config['path'])
+
+# abstract processor
+class Processor:
+
+    def process(self):
+        print('-' * 37)
+        print('-', self.processor_name)
+        print('-' * 37)
+        self.do_process()
+        print('Done.')
+        print()
+
+    def do_process(self):
+        pass
+
+# PROCESSOR: VCSProcessor
+class VCSProcessor(Processor):
+
+    def __init__(self, workspace_path, config):
+        self.processor_name = 'VCS Processor'
+        self.workspace_path = workspace_path
+        self.config = config
+
+    def do_process(self):
+        print('aaaaaaaaaaaaaa')
 
 
-    # initialize patch output directory'
-    init_patch_dir(DFT_PATCH_ABS_PATH)
 
-    # compare 2 builds and generate patch'
-    cmp_dir(
-        DFT_BASE_BUILD_ABS_PATH,
-        DFT_TARGET_BUILD_ABS_PATH,
-        DFT_PATCH_ABS_PATH,
-        [diff_output_handler]
-    )
+# PROCESSOR: BUILDProcessor
+class BUILDProcessor(Processor):
 
+    def __init__(self, workspace_path, config):
+        self.processor_name = 'BUILD Processor'
+        self.workspace_path = workspace_path
+        self.config = config
 
-# initialize patch directory (just clean the directory)
-def init_patch_dir(path):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-    os.mkdir(path)
+    def do_process(self):
+        for build in self.config['builds']:
+            cmd = 'mvn -s ' + os.path.join(self.workspace_path, self.config['mvn_setting'])
+            cmd += ' -f ' + os.path.join(self.workspace_path, build['path'], 'pom.xml')
+            cmd += ' ' + build['lp']
+            subprocess.run(cmd)
 
 
-# compare two directory and return the comparison result:
-#   {
-#       'common_dirs': [],
-#       'diff_files': [],
-#       'added_items': []
-#   }
-def cmp_dir(base_path, target_path, patch_path, handlers):
-    cmp_result = {}
-    cmp_obj = filecmp.dircmp(base_path, target_path, [])
 
-    cmp_result['common_dirs'] = cmp_obj.common_dirs
-    cmp_result['diff_files'] = cmp_obj.diff_files
-    cmp_result['added_items'] = cmp_obj.right_only
+# PROCESSOR: PATCHProcessor
+class PATCHProcessor(Processor):
 
-    # processing handler, passing the comparing result to handlers
-    for handler in handlers:
-        handler(cmp_result, base_path, target_path, patch_path)
+    def __init__(self, workspace_path, config):
+        self.processor_name = 'PATCH Processor'
+        self.workspace_path = workspace_path
+        self.config = config
+        self.handlers = [
+            self._diff_output_handler
+        ]
 
-    # recursively process common directory
-    for common_dir in cmp_obj.common_dirs:
-        cmp_dir(
-            os.path.join(base_path, common_dir),
-            os.path.join(target_path, common_dir),
-            os.path.join(patch_path, common_dir),
-            handlers
+
+    def do_process(self):
+
+        base_path = os.path.join(self.workspace_path, self.config['base_path'])
+        dest_path = os.path.join(self.workspace_path, self.config['dest_path'])
+        output_path = os.path.join(self.workspace_path, self.config['output_path'])
+
+        if not os.path.exists(base_path):
+            raise Exception('missing base path: ' + base_path)
+        if not os.path.exists(dest_path):
+            raise Exception('missing dest path: ' + dest_path)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        else:
+            shutil.rmtree(output_path)
+            os.makedirs(output_path)
+
+        self._cmp_dir(
+            base_path,
+            dest_path,
+            output_path
         )
 
 
-# handler for output patch files
-def diff_output_handler(cmp_result, base_path, target_path, patch_path):
-    # if no diff files and added items, handler will do nothing
-    if len(cmp_result['diff_files']) == 0 and len(cmp_result['added_items']) == 0:
-        return
+    def _cmp_dir(self, base_path, dest_path, output_path):
+        cmp_result = {}
+        cmp_obj = filecmp.dircmp(base_path, dest_path, [])
+        cmp_result['diff_files'] = cmp_obj.diff_files
+        cmp_result['added_items'] = cmp_obj.right_only
+        for handler in self.handlers:
+            handler(cmp_result, base_path, dest_path, output_path)
+        for common_dir in cmp_obj.common_dirs:
+            self._cmp_dir(
+                os.path.join(base_path, common_dir),
+                os.path.join(dest_path, common_dir),
+                os.path.join(output_path, common_dir)
+            )
 
-    # if current directory does not exist, create it
-    if not os.path.exists(patch_path):
-        os.makedirs(patch_path)
 
-    # output diff files
-    for file in cmp_result['diff_files']:
-        shutil.copyfile(
-            os.path.join(target_path, file),
-            os.path.join(patch_path, file)
-        )
+    def _diff_output_handler(self, cmp_result, base_path, dest_path, output_path):
 
+        if len(cmp_result['diff_files']) == 0 and len(cmp_result['added_items']) == 0:
+            return
+
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        for file in cmp_result['diff_files']:
+            print('[U]', os.path.join(output_path, file))
+            shutil.copyfile(
+                os.path.join(dest_path, file),
+                os.path.join(output_path, file)
+            )
+
+        for item in cmp_result['added_items']:
+            item_dest_path = os.path.join(dest_path, item)
+            item_output_path = os.path.join(output_path, item)
+            if os.path.isdir(item_dest_path):
+                print('[A]', os.path.join(item_output_path, '*'))
+                shutil.copytree(item_dest_path, item_output_path)
+            if os.path.isfile(item_dest_path):
+                print('[A]', item_output_path)
+                shutil.copyfile(item_dest_path, item_output_path)
 
 main()
